3ed673f61bd0d1654b4a4f5f8e2901bf05d54d40
MemoryCrash <stackexchange@qq.com>
2017-03-27 18:36:12 +0800

    修改sigmoid图片中的说明信息

--- image/imageGenSigmoid.py
+++ image/imageGenSigmoid.py
@@ -14,7 +14,7 @@ X = np.linspace(-10, 10, 256, endpoint=True)
 Y = sigmoid(X)
 
 plt.plot(X, Y)
-plt.text(3, 0.5, r'$\sigma\left(z\right)=\frac{1}{1+e^{-z}}$', fontsize = 15)
+plt.text(3, 0.5, r'$g\left(z\right)=\frac{1}{1+e^{-z}}$', fontsize = 15)
 
 
 plt.show()
