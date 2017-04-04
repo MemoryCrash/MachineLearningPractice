53d339c1913b52ac46d98e8ca9faae58c9c9f887
MemoryCrash <stackexchange@qq.com>
2017-04-04 18:03:06 +0800

    添加smo算法的初始信息

--- svm/svmMLiA.py
+++ svm/svmMLiA.py
@@ -36,6 +36,84 @@ def clipAlpha(aj, H, L):
     return aj
 
 
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn)
+    lableMat = mat(classLabels).transpose()
+    b = 0
+    m, n = shape(dataMatrix)
+    alphas = mat(zeros(m, 1))
+    iter = 0
+
+    while (iter < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas, lableMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
+            Ei = fXi - float(lableMat[i])
+            if((lableMat[i] * Ei < -toler) and (alphas[i] < C)) or ((lableMat[i] * Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i, m)
+                fXj = float(multiply(alphas, lableMat).T*(dataMatrix*dataMatrix[j, :].T)) + b
+                Ej = fXj - float(lableMat[j])
+
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+
+                if (lableMat[i] != lableMat[j]):
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+
+                if L == H:
+                    print("L==H")
+                    continue
+
+                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - \
+                dataMatrix[i, :] * dataMatrix[i].T - \
+                dataMatrix[j, :] * dataMatrix[j, :].T
+
+                if eta >= 0:
+                    print("eta>=0")
+                    continue
+
+                alphas[i] -= lableMat[j] * (Ei - Ej) / eta
+                alphas[j] = clipAlpha(alphas[j], H, L)
+
+                if (abs(alphas[j] - alphaJold) < 0.00001):
+                    print("j not moving enough")
+                    continue
+
+                alphas[i] += lableMat[j] * lableMat[i] * (alphaJold - alphas[j])
+                b1 = b - Ei - lableMat[i] * (alphas[i] - alphaIold) * \
+                dataMatrix[i, :] * dataMatrix[i, :].T - \
+                lableMat[j] * (alphas[j] - alphaJold) * \
+                dataMatrix[i, :] * dataMatrix[j, :].T
+
+                b2 = b - Ej - lableMat[i] * (alphas[i] - alphaIold) * \
+                dataMatrix[i, :] * dataMatrix[j, :].T - \
+                lableMat[j] * (alphas[j] - alphaJold) * \
+                dataMatrix[j, :] * dataMatrix[j, :].T
+
+                if (0 < alphas[i]) and (C > alphas[i]):
+                    b = b1
+                elif(0 < alphas[j] and (C > alphas[j])):
+                    b = b2
+                else:
+                    b = (b1 + b2)/2.0
+
+                alphaPairsChanged += 1
+                print("iter:%d i :%d, pairs changed %d"% (iter, i, alphaPairsChanged))
+
+        if (alphaPairsChanged == 0):
+            iter += 1
+        else:
+            iter = 0
+
+        print("iteration number:%d"%iter)
+
+    return b, alphas
+
+
 if __name__ == '__main__':
     dataArr, labelArr = loadDataSet("./dataSource/testSet.txt")
     print(labelArr)
