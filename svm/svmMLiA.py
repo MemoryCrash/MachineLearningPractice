5a9ce1816d7cab71e0bfdeb6a52ea3e5f8a97581
MemoryCrash <stackexchange@qq.com>
2017-04-06 15:51:58 +0800

    添加svm代码

--- svm/svmMLiA.py
+++ svm/svmMLiA.py
@@ -88,7 +88,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                     continue
 
                 alphas[i] += lableMat[j] * lableMat[i] * (alphaJold - alphas[j])
-                
+
                 b1 = b - Ei - lableMat[i] * (alphas[i] - alphaIold) * \
                 dataMatrix[i, :] * dataMatrix[i, :].T - \
                 lableMat[j] * (alphas[j] - alphaJold) * \
@@ -119,10 +119,165 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     return b, alphas
 
 
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.lableMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m, 1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m, 2)))
+
+
+def calcEk(oS, k):
+    fXk = float(multiply(oS.alphas, oS.lableMat).T * (oS.X * oS.X[k, :].T)) + oS.b
+    Ek = fXk - float(oS.lableMat[k])
+    return Ek
+
+
+def updateEk(oS, k):
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1, Ek]
+
+
+def selectJ(i, oS, Ei):
+    maxK = -1
+    maxDeltaE = 0
+    Ej = 0
+    oS.eCache[i] = [1, Ei]
+    # 将输入的数字的非零的位置返回，反化一个元组，对于二维数据，元组中的数据就包含了行位置的列表和列位置的列表
+    # matrix.A 返回一个 ndarray 的自己
+    # [:,0]返回第一列的数据
+    validEcacheList = nonzero(oS.eCache[:, 0].A)[0]
+
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:
+            if k == i:
+                continue
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei - Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k
+                maxDeltaE = deltaE
+                Ej = Ek
+        return maxK, Ej
+    else:
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+
+    return j, Ej
+
+
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if (((oS.lableMat[i] * Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or\
+     ((oS.lableMat[i] * Ei > oS.tol) and (oS.alphas[i] > 0))):
+        j, Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy()
+        alphaJold = oS.alphas[j].copy()
+
+        if(oS.lableMat[i] != oS.lableMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+
+        if(L == H):
+            print("L==H")
+            return 0
+        eta = 2.0 * oS.X[i, :] * oS.X[j, :].T - oS.X[i,:] * oS.X[i, :].T - \
+        oS.X[j, :] * oS.X[j, :].T
+
+        if eta >= 0:
+            print("eta >= 0")
+            return 0
+
+        oS.alphas[j] -= oS.lableMat[j] * (Ei - Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001):
+            print("j not moving enough")
+            return 0
+
+        oS.alphas[i] += oS.lableMat[i] * oS.lableMat[i] * (alphaJold - oS.alphas[j])
+        updateEk(oS, i)
+
+        b1 = oS.b - Ei - oS.lableMat[i] * (oS.alphas[i] - alphaIold) * \
+        oS.X[i, :] * oS.X[i, :].T - oS.lableMat[j] * \
+        (oS.alphas[j] - alphaJold) * oS.X[i, :] * oS.X[j, :].T
+
+        b2 = oS.b - Ej - oS.lableMat[i] * (oS.alphas[i] - alphaIold) * \
+        oS.X[i, :] * oS.X[j, :].T - oS.lableMat[j] * \
+        (oS.alphas[j] - alphaJold) * oS.X[j, :] * oS.X[j, :].T
+
+        if(0 < oS.alphas[i] and oS.C > oS.alphas[i]):
+            oS.b = b1
+        elif(0 < oS.alphas[j] and oS.C > oS.alphas[j]):
+            oS.b = b2
+        else:
+            oS.b = (b1 + b2) / 2
+
+        return 1
+
+    else:
+        return 0
+
+
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup = ('lin', 0)):
+    oS = optStruct(mat(dataMatIn), mat(classLabels).transpose(), C, toler)
+    iter = 0
+    entireSet = True
+    alphaPairsChanged = 0
+
+    while(iter < maxIter and alphaPairsChanged > 0 or entireSet):
+        alphaPairsChanged = 0
+        if(entireSet):
+            for i in range(oS.m):
+                alphaPairsChanged += innerL(i, oS)
+                print("fullSet, iter:%d i:%d, paris changed %d" %(iter, i, alphaPairsChanged))
+            iter += 1
+        else:
+            nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i, oS)
+                print("non-bound, iter:%d i:%d, paris changed %d"%(iter, i, alphaPairsChanged))
+            iter += 1
+        if(entireSet):
+            entireSet = False
+        elif(alphaPairsChanged == 0):
+            entireSet = True
+        print("iteration number:%d"% iter)
+    return oS.b, oS.alphas
+
+
+def calcWs(alphas, dataArr, classLabels):
+    X = mat(dataArr)
+    lableMat = mat(classLabels).transpose()
+    m, n = shape(X)
+    w = zeros((n, 1))
+
+    for i in range(m):
+        w += multiply(alphas[i] * lableMat[i], X[i, :].T)
+
+    return w
+
+
 if __name__ == '__main__':
     dataArr, labelArr = loadDataSet("./dataSource/testSet.txt")
-    # print(labelArr)
-    b, alphas = smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
-    print("output")
+
+    b, alphas = smoP(dataArr, labelArr, 0.6, 0.001, 40)
     print(b)
-    print(alphas[alphas > 0])
+    print(alphas[alphas>0])
+    ws = calcWs(alphas, dataArr, labelArr)
+
+    datMat = mat(dataArr)
+    print(datMat[0] * mat(ws) + b)
+    print(labelArr[0])
+    print(datMat[1] * mat(ws) + b)
+    print(labelArr[1])
+    print(datMat[2] * mat(ws) + b)
+    print(labelArr[2])
