d156cde68087c6e9a2c9da41661a16275f7b52e6
MemoryCrash <stackexchange@qq.com>
2017-04-14 20:59:39 +0800

    添加图片转换为矩阵的函数

--- image/imge2mat.py
+++ image/imge2mat.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*-coding:UTF-8 -*-
-from scipy.misc import imread, imshow
+from PIL import Image
+import numpy as np
 
-img = imread('2.png')
-imshow()
+
+def rgb2gray(rgbImage):
+
+    grayImg = rgbImage.convert('L')
+    grayMat = np.matrix(grayImg.getdata())
+
+    data = (255 - np.reshape(grayMat, (784, 1))) / 255
+
+    return data
+
+
+if __name__ == '__main__':
+    rgbImage = Image.open('2.png')
+    grayData = rgb2gray(rgbImage)
+    print(grayData)
