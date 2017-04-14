88d15038712c7f08aeb7ff1798466ccd2638cdb2
Dailei <stackexchange@qq.com>
2017-04-14 17:28:46 +0800

    添加对象init函数

--- neural/neuralTrain.py
+++ neural/neuralTrain.py
@@ -5,7 +5,8 @@ import random
 import numpy as np
 
 class Network(object):
-    def __ini__(self, sizes):
+
+    def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
