bab44f2fc052d762f8db8376dcd5642f874dca8d
Dailei <stackexchange@qq.com>
2017-04-14 18:08:11 +0800

    添加修改后的文件

--- neural/dataLoader.py
+++ neural/dataLoader.py
@@ -18,13 +18,13 @@ def load_data_wrapper():
     tr_d, va_d, te_d = load_data()
     trainning_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     trainning_results = [vectorized_result(y) for y in tr_d[1]]
-    trainning_data = zip(trainning_inputs, trainning_results)
+    trainning_data = list(zip(trainning_inputs, trainning_results))
 
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
-    validation_data = zip(validation_inputs, va_d[1])
+    validation_data = list(zip(validation_inputs, va_d[1]))
 
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
-    test_data = zip(test_inputs, te_d[1])
+    test_data = list(zip(test_inputs, te_d[1]))
 
     return (trainning_data, validation_data, test_data)
 
--- neural/neuralTrain.py
+++ neural/neuralTrain.py
@@ -23,9 +23,9 @@ class Network(object):
             n_test = len(test_data)
         n = len(training_data)
 
-        for j in xrange(epochs):
+        for j in range(epochs):
             random.shuffle(training_data)
-            mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
 
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
@@ -37,12 +37,13 @@ class Network(object):
 
     def update_mini_batch(self, mini_batch, eta):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w,shape) for w in self.weights]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
 
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
 
         self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
@@ -50,6 +51,7 @@ class Network(object):
     def backprop(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+
         activation = x
         activations = [x]
         zs = []
@@ -60,16 +62,16 @@ class Network(object):
             activation = sigmoid(z)
             activations.append(activation)
 
-        delta = self.cost_drivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
-        for l in xrange(2, self.num_layers):
+        for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
             delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
             nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l + 1].transpose())
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
 
         return (nabla_b, nabla_w)
 
