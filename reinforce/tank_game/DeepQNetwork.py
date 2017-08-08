5b5573ba4e8ee48bf880d17212ace1ea0400a508
MemoryCrash <stackexchange@qq.com>
2017-08-08 21:17:49 +0800

    添加一些必要注释信息

--- reinforce/tank_game/DeepQNetwork.py
+++ reinforce/tank_game/DeepQNetwork.py
@@ -3,23 +3,26 @@
 
 import cv2
 import random
-import numpy as np 
-import tensorflow as tf 
+import numpy as np
+import tensorflow as tf
 from collections import deque
 
-from tanksbattle import Tanks
 
 def image_chg(img):
+    """
+    对图片的RGB通道进行调整从BGR迅速调整为RGB顺序并对图片先旋转再对折
+    """
     b, g, r = cv2.split(img)
     return cv2.merge([r.transpose(), g.transpose(), b.transpose()])
+
 
 class DeepQNetwork:
 
     def _network(self, c_names):
-        w_initializer, b_initializer = tf.random_normal_initializer(0., 0.01), tf.constant_initializer(0.01) 
-        
-        conv2d = lambda x, W, stride: tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
-        max_pool_2x2 = lambda x: tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+        w_initializer = tf.random_normal_initializer(0., 0.01)
+        b_initializer = tf.constant_initializer(0.01)
+        conv2d = lambda x, W, stride: tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding="SAME")
+        max_pool_2x2 = lambda x: tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
         # 卷积层1
         with tf.variable_scope('conv1'):
@@ -38,8 +41,8 @@ class DeepQNetwork:
         with tf.variable_scope('conv3'):
             wc3 = tf.get_variable('w_c3', [3, 3, 64, 64], initializer=w_initializer, collections=c_names)
             bc3 = tf.get_variable('b_c3', [64], initializer=b_initializer, collections=c_names)
-            h_conv3 = tf.nn.relu(conv2d(h_conv2, wc3, 1) + bc3)           
-            h_conv3_flat = tf.reshape(h_conv3, [-1, 1600]) 
+            h_conv3 = tf.nn.relu(conv2d(h_conv2, wc3, 1) + bc3)
+            h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
 
         # 第一层. collections 是在更新 target_net 参数时会用到
         with tf.variable_scope('l1'):
@@ -55,28 +58,32 @@ class DeepQNetwork:
 
         return value
 
-
     def _build_net(self):
         # -------------- 创建 eval 神经网络, 及时提升参数 --------------
-        self.s = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 4], name='s')  # 用来接收 observation
-        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target') # 用来接收 q_target 的值, 这个之后会通过计算得到
+        # 用来接收 observation
+        self.s = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 4], name='s')
+        # 用来接收 q_target 的值, 这个之后会通过计算得到
+        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')
         with tf.variable_scope('eval_net'):
             # c_names(collections_names) 是在更新 target_net 参数时会用到
             c_names = ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
             self.q_eval = self._network(c_names)
 
-        with tf.variable_scope('loss'): # 求误差
+        # 求误差
+        with tf.variable_scope('loss'):
             self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
-        with tf.variable_scope('train'):    # 梯度下降
+        # 梯度下降
+        with tf.variable_scope('train'):
             self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
 
+
         # ---------------- 创建 target 神经网络, 提供 target Q ---------------------
-        self.s_ = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 4], name='s_')    # 接收下个 observation
+        # 接收下个 observation
+        self.s_ = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 4], name='s_')
         with tf.variable_scope('target_net'):
             # c_names(collections_names) 是在更新 target_net 参数时会用到
             c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
             self.q_next = self._network(c_names)
-
 
     def __init__(
         self,
@@ -90,17 +97,29 @@ class DeepQNetwork:
         batch_size=32,
         e_greedy_increment=None,
         output_graph=False,
-):
+        ):
         self.n_actions = n_actions
         self.image_size = image_size
         self.lr = learning_rate
         self.gamma = reward_decay
-        self.epsilon_max = e_greedy     # epsilon 的最大值
-        self.replace_target_iter = replace_target_iter  # 更换 target_net 的步数
-        self.memory_size = memory_size  # 记忆上限
-        self.batch_size = batch_size    # 每次更新时从 memory 里面取多少记忆出来
-        self.epsilon_increment = e_greedy_increment # epsilon 的增量
-        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max # 是否开启探索模式, 并逐步减少探索次数
+
+        # epsilon 的最大值
+        self.epsilon_max = e_greedy
+
+        # 更换 target_net 的步数
+        self.replace_target_iter = replace_target_iter
+
+        # 记忆上限
+        self.memory_size = memory_size
+
+        # 每次更新时从 memory 里面取多少记忆出来
+        self.batch_size = batch_size
+
+        # epsilon 的增量
+        self.epsilon_increment = e_greedy_increment
+
+        # 是否开启探索模式, 并逐步减少探索次数
+        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
 
         # 记录学习次数 (用于判断是否更换 target_net 参数)
         self.learn_step_counter = 0
@@ -119,46 +138,49 @@ class DeepQNetwork:
             tf.summary.FileWriter("logs_tank/", self.sess.graph)
 
         self.sess.run(tf.global_variables_initializer())
-        self.cost_his = []  # 记录所有 cost 变化, 用于最后 plot 出来观看
+        # 记录所有 cost 变化, 用于最后 plot 出来观看
+        self.cost_his = []
+        # 检查是否存在网络可以恢复
         self.net_restore()
 
     def store_transition(self, s, a, r, s_, t):
 
-        # 记录一条 [s, a, r, s_] 记录
+        # 从memory右边添加记录一条 [s, a, r, s_, t] 记录
+        # 如果数据超过了memorysize的显示就将左边的数据弹出
         self.memory.append((s, a, r, s_, t))
         if len(self.memory) > self.memory_size:
             self.memory.popleft()
-
-
 
     def choose_action(self, observation, step, OBSERVE):
         # 统一 observation 的 shape (1, size_of_observation)
         observation = observation[np.newaxis, :]
         action = np.zeros(self.n_actions)
 
+        # 按照epsilon和OBSERVE来判断是随机选择一个动作还是按照神经网络来选择一个最大q值的动作
+        # 随机选择是为了可以获得更多的可能
         if np.random.uniform() < self.epsilon and step > OBSERVE:
             # 让 eval_net 神经网络生成所有 action 的值, 并选择值最大的 action
             actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
 
-            action[np.argmax(actions_value)]=1
+            action[np.argmax(actions_value)] = 1
         else:
-            action[np.random.randint(0, self.n_actions)] = 1   # 随机选择
-            #print('random choice:{}'.format(step))
+            action[np.random.randint(0, self.n_actions)] = 1
 
         return action
 
-
     def _replace_target_params(self):
         # 使用 Tensorflow 中的 assign 功能替换 target_net 所有参数
-        t_params = tf.get_collection('target_net_params')   # 提取 target_net 的参数
-        e_params = tf.get_collection('eval_net_params') # 提取  eval_net 的参数
-        self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])  # 更新 target_net 参数
+        # 提取 target_net 的参数
+        t_params = tf.get_collection('target_net_params')
+        # 提取  eval_net 的参数
+        e_params = tf.get_collection('eval_net_params')
+        # 更新 target_net 参数
+        self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])
 
     def learn(self):
         # 检查是否替换 target_net 参数
         if self.learn_step_counter % self.replace_target_iter == 0:
             self._replace_target_params()
-            #print('\ntarget_params_replaced\n')
 
         # 从 memory 中随机抽取 batch_size 这么多记忆
         batch_memory = random.sample(self.memory, self.batch_size)
@@ -180,21 +202,15 @@ class DeepQNetwork:
             if batch_memory[i][4]:
                 q_target[i][np.argmax(batch_a[i])] = batch_r[i]
             else:
-                q_target[i][np.argmax(batch_a[i])] \
-                = batch_r[i] + self.gamma * np.max(q_next[i])
-
-        #eval_act_index = np.argmax(batch_a, axis=1)
-        #reward = batch_r
-        #batch_index = np.arange(self.batch_size, dtype=np.int32)
-        #q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
-
+                q_target[i][np.argmax(batch_a[i])] = \
+                batch_r[i] + self.gamma * np.max(q_next[i])
 
         # 训练 eval_net
         _, self.cost = self.sess.run([self._train_op, self.loss],
                                      feed_dict={self.s: batch_s,
-                                                self.q_target: q_target})
-
-        self.cost_his.append(self.cost) # 记录 cost 误差
+                                     self.q_target: q_target})
+        # 记录 cost 误差
+        self.cost_his.append(self.cost)
 
         # 逐渐增加 epsilon, 降低行为的随机性
         self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
@@ -209,7 +225,7 @@ class DeepQNetwork:
 
     def net_saver(self, step):
         if step % 50000 == 0:
-            self.saver.save(self.sess, 'saved_networks_tank/' + 'tank-dqn', global_step = step)
+            self.saver.save(self.sess, 'saved_networks_tank/tank-dqn', global_step=step)
 
     def net_restore(self):
         checkpoint = tf.train.get_checkpoint_state("saved_networks_tank")
--- reinforce/tank_game/Main.py
+++ reinforce/tank_game/Main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # coding=utf-8
-import sys 
+import sys
 
 import cv2
 import time
-import numpy as np 
+import numpy as np
 from datetime import timedelta
 
 from tanksbattle import Tanks
@@ -14,34 +14,48 @@ ACTIONS = 6
 IMAGE_SIZE = 80
 OBSERVE = 10000
 
+
 def print_progress(episode, episode_max, start_time):
+    """
+    打印花费的时间以及显示玩了多少回合
+    """
     current_time = time.time()
     time_dif = current_time - start_time
     use_time = str(timedelta(seconds=int(round(time_dif))))
-    msg = "\r- episode:{}/{} - Use time:{}".format(episode, episode_max, use_time)
+
+    msg = "\r- episode:{}/{} - Use time:{}"
+    msg = msg.format(episode, episode_max, use_time)
 
     sys.stdout.write(msg)
     sys.stdout.flush()
 
-def env_reset(env):
-        do_nothing = np.zeros(ACTIONS)
-        do_nothing[0] = 1
-        x_t, r_0, terminal = env.frame_step(do_nothing)
-        x_t = image_chg(x_t)
 
-        x_t = cv2.cvtColor(x_t, cv2.COLOR_BGR2GRAY)
-        return np.stack((x_t, x_t, x_t, x_t), axis = 2)
+def env_reset(env):
+    """
+    给出游戏开始初始的第一个环境图片
+    """
+    do_nothing = np.zeros(ACTIONS)
+    do_nothing[0] = 1
+    x_t, r_0, terminal = env.frame_step(do_nothing)
+    x_t = image_chg(x_t)
+
+    x_t = cv2.cvtColor(x_t, cv2.COLOR_BGR2GRAY)
+    return np.stack((x_t, x_t, x_t, x_t), axis=2)
+
 
 def next_observation(raw_observation_, observation):
+    """
+    对动作作用后的原始的环境帧图片进行处理，将上个环境图片添加三张进去
+    """
     raw_observation_ = cv2.cvtColor(raw_observation_, cv2.COLOR_BGR2GRAY)
-    #cv2.imwrite('./images_tank/1.jpg', raw_observation_)
-    #ret, raw_observation_ = cv2.threshold(raw_observation_,1,255,cv2.THRESH_BINARY)
     raw_observation_ = np.reshape(raw_observation_, (80, 80, 1))
-    return np.append(raw_observation_, observation[:,:,0:3], axis = 2)
-
+    return np.append(raw_observation_, observation[:, :, 0:3], axis=2)
 
 
 def run_tanks():
+    """
+    运行坦克大战游戏
+    """
     step = 0
     play_time = 1000
 
@@ -51,23 +65,30 @@ def run_tanks():
         episode_reward = 0
 
         while True:
-
+            # 选择动作
             action = RL.choose_action(observation, step, OBSERVE)
+
+            # 施加选择的动作到环境上，环境返回动作作用后环境信息，当前动作奖励，环境状态(结束true/继续false)
             raw_observation_, reward, terminal = env.frame_step(action)
-            episode_reward += reward
             raw_observation_ = image_chg(raw_observation_)
             observation_ = next_observation(raw_observation_, observation)
+
+            # 将以上的环境，动作，动作反馈，动作作用后的环境，环境状态存入队列，后续训练使用
             RL.store_transition(observation, action, reward, observation_, terminal)
 
             if step == OBSERVE:
                 print('\nMemory is enough begin to train')
 
+            # 先运行一段时间收集足够的训练信息再开始学习
             if(step > OBSERVE) and (step % 5 == 0):
                 RL.learn()
 
+            # 保存训练的神经网络
             RL.net_saver(step)
 
+            # 将环境进行转换，动作作用后的环境变成了当前环境
             observation = observation_
+            episode_reward += reward
 
             if terminal:
                 break
@@ -91,7 +112,4 @@ if __name__ == '__main__':
         )
 
     run_tanks()
-    #RL.plot_cost()
     print('\n')
-
-
